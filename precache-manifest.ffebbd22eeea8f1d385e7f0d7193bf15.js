adda8e48c33ab76d33bdcdddc99a9d86df85b10e
Himanshusinghofficial <hs882677@gmail.com>
2020-12-01 19:36:55 +0530

    Updates

---
+++ precache-manifest.ffebbd22eeea8f1d385e7f0d7193bf15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b96cffd520560076b974416794806275",
+    "url": "/index.html"
+  },
+  {
+    "revision": "463ea52c3955ae18e344",
+    "url": "/static/css/main.54a70f98.chunk.css"
+  },
+  {
+    "revision": "f25cd917904166e4b7c4",
+    "url": "/static/js/2.a021d05a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a021d05a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "463ea52c3955ae18e344",
+    "url": "/static/js/main.e73206d9.chunk.js"
+  },
+  {
+    "revision": "9f10ad7796608ffb84b7",
+    "url": "/static/js/runtime-main.1b50fb31.js"
+  }
+]);
